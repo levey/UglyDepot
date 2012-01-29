c2f87c2f2cf6954eb2a024e94b255611ab7849e4
levey <levey.zhu@gmail.com>
2012-01-29 15:28:40 +0800

    Validation

--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -1,7 +1,53 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
+  # test "product attributes must not be empty" do product = Product.new
+  #   assert product.invalid?
+  #   assert product.errors[:title].any?
+  #   assert product.errors[:description].any?
+  #   assert product.errors[:price].any?
+  #   assert product.errors[:image_url].any?
   # end
+  # test "product price must be positive" do
+  #   product = Product.new(:title => "My Book Title",
+  #                         :description => "Book Description",
+  #                         :image_url => "xx.jpg")
+  #   product.price = -1
+  #   assert product.invalid?
+  #   assert_equal "must be greater than or equal to 0.01", product.errors[:price].join('; ')
+  # end
+  # 
+  # def new_product(image_url)
+  #   Product.new(:title => "My Book Title",
+  #               :description => "Book Description",
+  #               :price => 1,
+  #               :image_url => image_url)
+  # end
+  # 
+  # test "image_url" do
+  #   ok = %w{fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg http://a.b.c/x/y/z/fred.gif}
+  #   bad = %w{ fred.doc fred.gif/more fred.gif.more }
+  #   
+  #   ok.each do |name|
+  #     assert new_product(name).valid?, "#{name} shouldn't be invailid "
+  #   end
+  #   
+  #   bad.each do |name|
+  #     assert new_product(name).valid?, "#{name} shouldn't be invailid "
+  #   end
+  # end
+  #     
+  
+  test "product is not valid without a unique title - i18n" do
+    product = Product.new(:title => products(:ruby).title,
+                          :description => "yyy", 
+                          :price => 1, 
+                          :image_url => "fred.gif")
+    assert !product.save
+    assert_equal I18n.translate('activerecord.errors.messages.taken'),
+    product.errors[:title].join('; ')
+  end
+               
+      
+  
 end
