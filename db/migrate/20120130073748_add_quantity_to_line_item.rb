0fb460fc51558d01fa6c96696fccb1a4ad7dce00
levey <levey.zhu@gmail.com>
2012-01-30 18:34:03 +0800

    Finished cart

---
+++ db/migrate/20120130073748_add_quantity_to_line_item.rb
@@ -0,0 +1,6 @@
+class AddQuantityToLineItem < ActiveRecord::Migration
+  def change
+    add_column :line_items, :quantity, :integer, :default => 1
+
+  end
+end
---
+++ db/migrate/20120130074806_combine_items_in_cart.rb
@@ -0,0 +1,26 @@
+class CombineItemsInCart < ActiveRecord::Migration
+  def up
+    Cart.all.each do |cart|
+      sums = cart.line_items.group(:product_id).sum(:quantity)
+      sums.each do |product_id, quantity|
+        if quantity > 1
+          # remove indicidual items
+          cart.line_items.where(:product_id => product_id).delete_all
+          
+          # replace with a single item
+          cart.line_items.create(:product_id => product_id, :quantity => quantity)
+        end
+      end
+    end          
+  end
+
+  def down 
+    LineItem.where("quantity>1").each do |lineitem|
+      lineitem.quantity.times do
+        LineItem.create :cart_id => lineitem.cart_id, 
+        :product_id => lineitem.product_id, :quantity => 1       
+      end
+      lineitem.destroy
+    end   
+  end
+end
